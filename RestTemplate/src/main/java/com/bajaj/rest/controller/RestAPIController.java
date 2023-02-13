7acffbbf959454a4c3ba12af2278eec5ddd6f1ad
Niraj <nirajnpatil517@gmail.com>
2023-02-13 09:38:13 +0530

    Rest

--- RestTemplate/src/main/java/com/bajaj/rest/controller/RestAPIController.java
+++ RestTemplate/src/main/java/com/bajaj/rest/controller/RestAPIController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,40 +17,54 @@ import org.springframework.web.client.RestTemplate;
 import com.bajaj.rest.entity.EmployeeBean;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RestAPIController {
 
 
-    private static RestTemplate restTemplate = new RestTemplate();
-
+    
+	public static RestTemplate restTemplate1 = new RestTemplate();
     @GetMapping("/getEmployees")
-    public static ResponseEntity<String> getEmployees() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+    public static List<EmployeeBean> getEmployees() {
+    	  RestTemplate restTemplate = new RestTemplate();
+    	  String resourceUrl = "http://localhost:8080/allEmployees";
 
-        ResponseEntity<String> result = restTemplate.exchange("http://localhost:8080/allEmployees", HttpMethod.GET, entity, String.class);
-        return result;
+         ResponseEntity<List> response = restTemplate.getForEntity(resourceUrl, List.class);
+        
+         List<EmployeeBean> result = response.getBody();
+    	return result;
     }
 
     @PostMapping("/addEmployee")
-    public String addEmployee(@RequestBody EmployeeBean employeeData) {
+    public String addEmployee(@RequestBody EmployeeBean employeeBean) {
+    	RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<EmployeeBean> entity = new HttpEntity<EmployeeBean>(employeeData, headers);
+        HttpEntity<EmployeeBean> entity = new HttpEntity<EmployeeBean>(employeeBean, headers);
 
         return restTemplate.exchange("http://localhost:8080/employeeAdd", HttpMethod.POST, entity, String.class).getBody();
-
+        
+        
+        
     }
 
     @DeleteMapping("/deleteEmployee/{eid}")
-    public Integer deleteEmployee(@PathVariable("eid") Integer eid) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<EmployeeBean> entity = new HttpEntity<EmployeeBean>(headers);
-
-        return restTemplate.exchange("http://localhost:8080/deleteId/{eid}", HttpMethod.DELETE, entity, Integer.class).getBody();
-
+    public String deleteEmployee(@PathVariable(value = "eid") int eid) {
+    	RestTemplate restTemplate = new RestTemplate();
+    	String url = "http://localhost:8080/deleteId/{eid}";
+    	Map<String, String> params = new HashMap<String,String>();
+    	params.put("eid", eid+"");
+    	restTemplate1.delete(url,params);
+    	return "Employee Deleted";
+    }
+    @PutMapping("/updateEmployee/{eid}")
+    public List<EmployeeBean> updateEmployee(@PathVariable(value = "eid") int eid)
+    {
+    	RestTemplate restTemplate = new RestTemplate();
+    	String url = "http://localhost:8080/deleteId/{eid}";
+		return null;
     }
 }
--- RestTemplate/src/main/java/com/bajaj/rest/entity/EmployeeBean.java
+++ RestTemplate/src/main/java/com/bajaj/rest/entity/EmployeeBean.java
@@ -1,39 +1,42 @@
 package com.bajaj.rest.entity;
 
-import lombok.Data;
 
-@Data
 public class EmployeeBean {
-    private int eid;
+    
+	private int eid;
     private String name;
     private int salary;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public EmployeeBean(int eid, String name, int salary) {
+		super();
+		this.eid = eid;
+		this.name = name;
+		this.salary = salary;
+	}
+	public EmployeeBean() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "EmployeeBean [eid=" + eid + ", name=" + name + ", salary=" + salary + "]";
+	}
     
-    
-//	public EmployeeBean() {
-//		super();
-//	}
-//	public EmployeeBean(int eid, String name, int salary) {
-//		super();
-//		this.eid = eid;
-//		this.name = name;
-//		this.salary = salary;
-//	}
-//	public int geteId() {
-//		return eid;
-//	}
-//	public void seteId(int eid) {
-//		this.eid = eid;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public int getSalary() {
-//		return salary;
-//	}
-//	public void setSalary(int salary) {
-//		this.salary = salary;
-//	}
+
 }
